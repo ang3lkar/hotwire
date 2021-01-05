9ef6082f6c34430fab8321ec4f643c663b09c76a
Angelos Karagkiozidis <angelkar@gmail.com>
2021-01-05 21:04:33 +0200

    simplify broadcast callbacks

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
-  after_create_commit -> { broadcast_append_to room }
-  after_destroy_commit -> { broadcast_append_to room }
-  after_update_commit -> { broadcast_append_to room }
+  # after_create_commit -> { broadcast_append_to room }
+  # after_destroy_commit -> { broadcast_append_to room }
+  # after_update_commit -> { broadcast_append_to room }
+  broadcasts_to :room
 end
